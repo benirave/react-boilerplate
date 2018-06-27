3ca3a69fbbdf88e1979eecab32e31790f01c7454
Bence Eros <bence.eros@nexta.io>
2018-06-28 00:00:48 +0200

    second commit

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../Reducers/index';
-import thunk from 'redux-thunk';
-
-export const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-)
